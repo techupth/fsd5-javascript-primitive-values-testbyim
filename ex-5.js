d19e1b36db99cc0599431a191ec7bab23fe33970
CodePawsDev <isripen1991@outlook.com>
2023-11-17 13:45:20 +0700

    just test to prove it working or not

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,4 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let test2 = "test again";
--- ex-6.js
+++ ex-6.js
@@ -4,3 +4,4 @@ let totalParts = 13000;
 
 let concatSentence;
 let literalSentence;
+let test2 = "test again";
